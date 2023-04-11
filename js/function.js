f3da8c95ad47150d9a1e0b245d290561b97c83db
Fullganne <firewolf148@gmail.com>
2023-04-11 12:26:51 +0700

    fix-show-hide-password

---
+++ js/function.js
@@ -0,0 +1,10 @@
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye-slash fa-eye");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
