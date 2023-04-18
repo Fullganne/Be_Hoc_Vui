a954f373e58717a9f71b222e7ebc3cdbc8152921
Fullganne <firewolf148@gmail.com>
2023-04-18 18:59:07 +0700

    update-check-confirm-password

--- js/login_register.js
+++ js/login_register.js
@@ -1,10 +1,12 @@
-const createPw = document.querySelector("#create_pw"),
+const form = document.getElementById("myForm"),
+  createPw = document.querySelector("#create_pw"),
   confirmPw = document.querySelector("#confirm_pw"),
   loginPw = document.querySelector("#login_pw")
   pwShow = document.querySelector(".toggle-password"),
   alertIcon = document.querySelector(".error"),
   alertText= document.querySelector(".text"),
-  submitBtn = document.querySelector(".submit-btn");
+  alertAll = document.querySelector(".alert"),
+  submitBtn = document.querySelector("#regis-submit-btn");
 // Register Form
   //Hide/show password
 pwShow.addEventListener("click", ()=>{
@@ -18,13 +20,14 @@ pwShow.addEventListener("click", ()=>{
     pwShow.classList.replace("fa-eye","fa-eye-slash");
   }
 });
+//Check input password at least 8 character
 createPw.addEventListener("input", ()=>{
   let val = createPw.value.trim()
   if(val.length >= 8){
     confirmPw.removeAttribute("disabled");
     submitBtn.removeAttribute("disabled");
     submitBtn.classList.add("active");
-    document.querySelector(".alert").style.display = "none";
+    alertAll.style.display = "none";
   }else {
     confirmPw.setAttribute("disabled", true);
     submitBtn.setAttribute("disabled", true);
@@ -32,20 +35,33 @@ createPw.addEventListener("input", ()=>{
     confirmPw.value = "";
     alertText.style.color = "#D93025";
     alertText.innerText = "Nhập ít nhất 8 kí tự";
-    document.querySelector(".alert").style.display = "flex";
+    alertAll.style.display = "flex";
   }
 });
-submitBtn.addEventListener("click", ()=>{
-  if(createPw.value === confirmPw.value){
-    alertText.innerText = "Password matched";
-    alertIcon.style.display = "none";
-    alertText.style.color = "#4070F4";
-  }else {
-    alertText.innerText = "Password didn't matched";
-    alertIcon.style.display = "block";
-    alertText.style.color = "#D93025";
+//Check createpw and confirmpw are the same
+const password = form.password;
+const confirmPassword = form.passwordAgain;
+const feedback = document.getElementsByClassName("alert");
+let isPasswordMatch = false;
+confirmPassword.addEventListener("input", () => {
+  if (password.value != confirmPassword.value) {
+    alertText.innerText = "Mật khẩu xác nhận chưa chính xác!";
+    alertAll.style.display = "flex";
+    isPasswordMatch = false;
+  } else {
+    feedback.innerHTML = "";
+    alertAll.style.display = "none";
+    isPasswordMatch = true;
   }
 });
+form.onsubmit = function () {
+  if (isPasswordMatch) {
+    alert("Form submitted. Great!");
+    return true;
+  }
+  alert("Wait! Password did not match.");
+  return false;
+};
 // Login Form
 pwShow.addEventListener("click", ()=>{
   if((loginPw.type === "password")){
