400da98a43c799f7deed8cd1682ca0d14f04f835
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2023-04-26 16:43:20 +0700

    prototype calculator with correct/incorrect signaling

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,4 @@
 const topics = document.querySelectorAll(".topic-item");
-console.log(topics);
 
 topics.forEach((ele) => {
     ele.addEventListener("click", () => {
@@ -26,3 +25,52 @@ function AllTopics(state) {
 
     return;
 };
+
+
+
+
+
+const mainContainer = document.getElementById("main-container");
+const calculator = document.getElementById("calculator");
+
+const equation = document.getElementById("equation");
+const finalResult = document.getElementById("user-answer");
+
+const correctIndicator = document.getElementById("correct");
+const incorrectIndicator = document.getElementById("incorrect");
+
+/* -------------------- LOGIC -------------------- */
+const operatorList = ["+", "-", "*", "/"];
+
+let wantedResult = randomRange(0, 9);
+
+
+finalResult.innerHTML = wantedResult;
+
+function openCalculator(grade = 1) {
+
+}
+
+
+
+
+function userSubmit(value) {
+    if (value == wantedResult) {
+        correctIndicator.classList.add("active");
+        setTimeout(() => {
+            correctIndicator.classList.remove("active")} 
+        , 1500);
+        return;
+    }
+
+
+    incorrectIndicator.classList.add("active");
+    setTimeout(() => {
+        incorrectIndicator.classList.remove("active")} 
+    , 1500);
+    return;
+}
+
+setTimeout(() => {
+    document.getElementById("calculator").scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+}, 1000);
--- js/main-script.js
+++ js/main-script.js
@@ -2,3 +2,12 @@
 // index, account, toan_1
 
 // git commit -m"modify whole styling structure and change about.html to introduce to prevent misunderstanding"
+
+const randomRange = (min, max) => Math.floor(Math.random()*(max-min)) + min;
+
+function swap(a, b) {
+    let temp = a;
+    a = b;
+    b = temp;
+    return;
+}
